ca79cac2fb4d25b447ab9899cbc08fd52724ec6c
Jan_Ernst <jan.ernst1910@gmail.com>
2024-12-16 17:02:42 +0100

    last font fix

--- src/pages/use-cases/CaseTemplate.js
+++ src/pages/use-cases/CaseTemplate.js
@@ -7,7 +7,7 @@ export default function CaseTemplate ({ imgSrc, title, detailDescription, childr
             <div className="flex-col m-20 w-[90vw] lg:w-[70vw] max-w-[1200px]">
                 <Link className="flex gap-2 my-2" href={'/use-cases'}>
                     <svg className="w-4 -rotate-90" data-name="1-Arrow Up" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="m26.71 10.29-10-10a1 1 0 0 0-1.41 0l-10 10 1.41 1.41L15 3.41V32h2V3.41l8.29 8.29z"/></svg>
-                    <p>Zurück</p>
+                    <p className="text-black">Zurück</p>
                 </Link>
                 <Image className="w-full rounded-lg" src={imgSrc} width={500} height={300} alt={'Photo'}/>
                 <p className="text-2xl text-black mt-8 font-bold">{title}</p>
